84479624b2bb007908afd454ad1d242884648e0b
Nadeem Anwar <ibul.aftab@proton.me>
2025-07-21 16:36:04 +0500

    - removed condition for checking console resolution

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdbool.h>
 #include <Windows.h>
 
-#define SCREEN_WIDTH	30
-#define	SCREEN_HEIGHT	100
-
-#define SCREEN_ERR		100
+#define SCREEN_WIDTH	50
+#define	SCREEN_HEIGHT	50
 
 static void SetWidnowsTitle(char *title)
 {
@@ -14,19 +13,11 @@ static void SetWidnowsTitle(char *title)
 static void SetNewBuffer(void)
 {
 	CONSOLE_SCREEN_BUFFER_INFO screen;
-	
+	HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
 	// new screen buffer
 	printf("\x1b[?1049h");
-	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screen))
-	{
-		// checks if the buffer dimensions are large enough
-		if (!(screen.dwSize.X < SCREEN_WIDTH | screen.dwSize.Y < SCREEN_HEIGHT))
-		{
-			fprintf(stderr, "SetNewBuffer: Screen is too small.\n");
-			exit(SCREEN_ERR);
-		}
-	}
-	else
+	if (!GetConsoleScreenBufferInfo(console_handle, &screen))
 	{
 		fprintf(stderr, "SetNewBuffer: Unable to get information of buffer.\n" "Exited with GetLastError.\n");
 		exit(GetLastError());
@@ -37,12 +28,6 @@ int main(void)
 {
 	SetWidnowsTitle("Tetris!");
 	SetNewBuffer();
-
-	printf(" _____    _        _     \n");
-	printf("|_   _|__| |_ _ __(_)___ \n");
-	printf("  | |/ _ \\ __| '__| / __|\n");
-	printf("  | |  __/ |_| |  | \\__ \\\n");
-	printf("  |_|\\___|\\__|_|  |_|___/\n");
 
 	char c = getch();
 	return 0;
